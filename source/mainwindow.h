d0cefb3e59cdc530c427cfc01fa111e2daf13f4b
596142041 <wcj53636746@126.com>
2017-09-20 20:53:58 +0800

    删除网友原有的关于CAN总线波特率的定义,添加了QStringList   chip_list全局变量 用于实现芯片的识别功能.

--- source/mainwindow.h
+++ source/mainwindow.h
@@ -13,6 +13,9 @@
 #include "usb2can.h"
 #include "ControlCAN.h"
 #include "can_driver.h"
+#include <QSettings>
+#include <QDir>
+#include <QStringList>
 #define DEBUG 1
 namespace Ui {
 class MainWindow;
@@ -27,47 +30,6 @@ public:
     ~MainWindow();
     int CAN_GetBaudRateNum(unsigned int BaudRate);
     int USB_CAN_status  = 0;
-#if 0
-    //定义CAN波特率参数表
-    typedef struct _CAN_BAUD_RATE{
-      unsigned char   SJW;
-      unsigned char   BS1;
-      unsigned char   BS2;
-      unsigned short  PreScale;
-      unsigned int    BaudRate;//前面4个参数对应的波特率，单位为Hz
-    } CAN_BAUD_RATE,*PCAN_BAUD_RATE;
-
-    CAN_BAUD_RATE  CANBaudRateTab[27]= {
-    //SJW,BS1,BS2,BRP,BAUD
-    {1,2,1,25,1000000},    // 1M
-    {1,2,1,28,900000},     // 900K
-    {1,3,1,25,800000},     // 800K
-    {1,3,1,30,666000},     // 666K
-    {1,4,1,28,600000},     // 600K
-    {1,6,1,25,500000},     // 500K
-    {1,8,1,25,400000},     // 400K
-    {1,7,1,37,300000},     // 300K
-    {1,6,1,50,250000},     // 250K
-    {1,3,1,89,225000},     // 225K
-    {1,16,3,25,200000},    // 200K
-    {1,6,1,78,160000},     // 160K
-    {1,15,2,37,150000},    // 150K
-    {1,6,1,87,144000},     // 144K
-    {1,6,1,100,125000},    // 125K
-    {1,6,1,104,120000},    // 120K
-    {1,16,3,50,100000},    // 100K
-    {1,6,1,139,90000},     // 90K
-    {1,1,3,278,80000},     // 80K
-    {1,6,1,167,75000},     // 75K
-    {1,6,1,208,60000},     // 60K
-    {1,6,1,250,50000},     // 50K
-    {1,6,1,312,40000},     // 40K
-    {1,6,1,417,30000},     // 30K
-    {1,6,1,625,20000},     // 20K
-    {1,13,2,625,10000},    // 10K
-    {1,16,3,1000,5000},    // 5K
-    };
-#endif
     typedef struct
         {
          unsigned char Timing0;
@@ -115,6 +77,7 @@ private slots:
     private:
     Ui::MainWindow *ui;
      Boot_CMD_LIST cmd_list;
+     QStringList   chip_list;
     int   CAN_BL_Nodecheck(int DevIndex,int CANIndex,unsigned short NodeAddr,unsigned int *pVersion,unsigned int *pType,unsigned int TimeOut);
     int   CAN_BL_init(PCBL_CMD_LIST pCmdList);
     int   CAN_BL_erase(int DevIndex,int CANIndex,unsigned short NodeAddr,unsigned int FlashSize,unsigned int TimeOut);
